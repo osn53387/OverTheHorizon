05dd3badaf8568bd61c7ced7629792a8d39fa00d
owen <osn53387@uga.edu>
2022-10-02 19:34:16 -0400

    Buttons created Bigger, revised HTML

--- OverTheHorizon.js
+++ OverTheHorizon.js
@@ -8,6 +8,7 @@ function startTime() {
     document.getElementById('Live Clock').innerHTML =  hours + ":" + minutes + ":" + seconds;
     setTimeout(startTime, 1000);
   }
+  
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
