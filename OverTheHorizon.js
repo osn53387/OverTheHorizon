9c40c0b82440f80653f8c898306d1d71772cf03d
owen <osn53387@uga.edu>
2022-10-01 20:34:55 -0400

    Added JS File, Implemented Clock and Buttons

---
+++ OverTheHorizon.js
@@ -0,0 +1,14 @@
+function startTime() {
+    const today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    document.getElementById('Live Clock').innerHTML =  hours + ":" + minutes + ":" + seconds;
+    setTimeout(startTime, 1000);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
