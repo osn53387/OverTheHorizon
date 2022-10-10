512b8fb436fa78143e342830ea9fea27d9418114
owen <osn53387@uga.edu>
2022-10-10 14:25:07 -0400

    Updated Sign-Up design

--- overthehorizion-frontend/src/components/sub-components/SignUp.jsx
+++ overthehorizion-frontend/src/components/sub-components/SignUp.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
+import "../sub-components-styling/SignUp.css";
 
 export class SignUp extends Component {
   render() {
     return (
       <div>
-        <div id="Sign-Up">
+        <form id="Sign-Up">
           <h1>New User</h1>
           <p>email</p>
           <input type="text" placeHolder="email" />
           <p>password</p>
           <input type="text" placeHolder="password" />
-          <button type="button">Register</button>
-        </div>
+          <button type="submit">Register</button>
+        </form>
       </div>
     );
   }
